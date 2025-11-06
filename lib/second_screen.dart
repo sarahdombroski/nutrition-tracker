ffe85d0931ae27196118c49b77a19eb71ea30f3a
sarahdombroski <dom22003@byui.edu>
2025-11-06 14:16:37 -0700

    changed second screen

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -7,14 +7,66 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("test"),
+        title: const Text("Saved Foods"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("test"),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                  style: BorderStyle.solid,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                      Text("Food Title", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      FloatingActionButton(
+                      onPressed: () {
+                        // Handle button press
+                      },
+                      tooltip: 'Add Food',
+                      mini: true,
+                      child: const Icon(Icons.add, color: Colors.black),
+                    )])
+                  ]), 
+                const Divider(thickness: 1),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start, 
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column( 
+                  crossAxisAlignment: CrossAxisAlignment.start, 
+                  children: [ 
+                    Text('Calories: 275'),
+                    Text('Protein: 20'),
+                    ]),),
+                    Expanded(
+                      flex: 1,
+                      child: Column( 
+                  crossAxisAlignment: CrossAxisAlignment.center, 
+                  children: [ 
+                    Text('Carbs: 15'),
+                    Text('Fat: 10'),
+                    ])),
+                    ])
+          ])),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
