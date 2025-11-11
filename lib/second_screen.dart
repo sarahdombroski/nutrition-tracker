9b3eca3acbae173e1f8799b53e6512f277373933
sarahdombroski <dom22003@byui.edu>
2025-11-11 13:52:52 -0700

    worked on second screen layout

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
+final food = [
+  {'name': 'Macaroni', 'calories': 275, 'protein': 20, 'carbs': 40, 'fat': 23},
+  {'name': 'Potato', 'calories': 250, 'protein': 10, 'carbs': 50, 'fat': 16},
+  {'name': 'Pancake', 'calories': 200, 'protein': 4, 'carbs': 42, 'fat': 10},
+];
+
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
+
+  void _addToList() {}
 
   @override
   Widget build(BuildContext context) {
@@ -10,73 +18,119 @@ class SecondScreen extends StatelessWidget {
         title: const Text("Saved Foods"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1.0,
-                  style: BorderStyle.solid,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: food.length,
+              itemBuilder: (context, index) {
+                final item = food[index];
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  padding: const EdgeInsets.all(8.0),
+                  margin: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                      Text("Food Title", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                      FloatingActionButton(
-                      onPressed: () {
-                        // Handle button press
-                      },
-                      tooltip: 'Add Food',
-                      mini: true,
-                      child: const Icon(Icons.add, color: Colors.black),
-                    )])
-                  ]), 
-                const Divider(thickness: 1),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start, 
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Column( 
-                  crossAxisAlignment: CrossAxisAlignment.start, 
-                  children: [ 
-                    Text('Calories: 275'),
-                    Text('Protein: 20'),
-                    ]),),
-                    Expanded(
-                      flex: 1,
-                      child: Column( 
-                  crossAxisAlignment: CrossAxisAlignment.center, 
-                  children: [ 
-                    Text('Carbs: 15'),
-                    Text('Fat: 10'),
-                    ])),
-                    ])
-          ])),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context); // Goes back to first screen
+                          Text(
+                            '${item['name']!}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  // Handle button press
+                                },
+                                tooltip: 'Delete from List',
+                                mini: true,
+                                child: const Icon(
+                                  Icons.delete,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(width: 8),
+                              FloatingActionButton(
+                                onPressed: () {
+                                  // Handle button press
+                                },
+                                tooltip: 'Edit Food',
+                                mini: true,
+                                child: const Icon(
+                                  Icons.edit,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(width: 8),
+                              FloatingActionButton(
+                                onPressed: () {
+                                  // Handle button press
+                                },
+                                tooltip: 'Add Food',
+                                mini: true,
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const Divider(thickness: 1),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Calories: ${item['calories']}'),
+                                Text('Protein: ${item['protein']}'),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text('Carbs: ${item['carbs']}'),
+                                Text('Fat: ${item['fat']}'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
               },
-              child: const Text("Go Back"),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addToList,
+        tooltip: 'Add Food to List',
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.list_alt, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
